e1768a1e22533f71281f26620ea2f377d7af529e
Aarya Rai <aarya.rai185@gmail.com>
2026-01-30 17:54:16 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // 1. IMPORT FIRESTORE
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVAe7QfXdsc0TNNYcbjyD_qRVigKJaO2A",
-  authDomain: "vedabloom-bc3c9.firebaseapp.com",
-  projectId: "vedabloom-bc3c9",
-  storageBucket: "vedabloom-bc3c9.firebasestorage.app",
-  messagingSenderId: "757997746657",
-  appId: "1:757997746657:web:50d8c41e779f88ecc9baa0",
-  measurementId: "G-N9L32D0K3W"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: :,
+  measurementId: 
 };
 
 // Initialize Firebase
